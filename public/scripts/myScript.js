bf396f8af3c083176e60785dbaf245a4dbf714cf
Desmond Chi <desmond.chi.g@gmail.com>
2024-09-18 17:18:59 -0400

    Add confetti on page load for letter page.

---
+++ public/scripts/myScript.js
@@ -0,0 +1,12 @@
+// import JSConfetti from "js-confetti";
+
+// const jsConfetti = new JSConfetti();
+
+// function sprayConfetti() {
+//   jsConfetti.addConfetti()
+// }
+
+// console.log("script found");
+// document.addEventListener("DOMContentLoaded", function() {
+//   sprayConfetti();
+// });
--- src/app/birthday_card/page.tsx
+++ src/app/birthday_card/page.tsx
@@ -1,20 +1,31 @@
-// import JSConfetti from 'js-confetti'
+"use client";
 
-// const jsConfetti = new JSConfetti();
-
-// function sprayConfetti() {
-//   jsConfetti.addConfetti();
-// }
+import JSConfetti from "js-confetti";
+import { useEffect, useCallback } from "react";
 
 export default function BirthdayCard() {
+
+  const sprayConfetti = useCallback(() => {
+    const jsConfetti = new JSConfetti();
+    jsConfetti.addConfetti();
+  }, []);
+
+  // Trigger confetti on page load/render.
+  useEffect(() => {
+    console.log("confetti");
+    sprayConfetti();
+  }, [sprayConfetti]);
+
   return (
     <div className="birthday_card_letter">
+      <button onClick={sprayConfetti}>Click for confetti</button>
       <p>Happy birthday, Nancy!</p>
       <br />
       <p>
         I hope you have an amazing day today. I wish I could be there in person
         with you, celebrating your 22nd birthday. I am celebrating your big day
-        3000 miles away with the best wishes of joy and happiness for you.  Thinking of you lots today (and always) and sending lots of love.
+        3000 miles away with the best wishes of joy and happiness for you.
+        Thinking of you lots today (and always) and sending lots of love.
       </p>
       <br />
       <p>
